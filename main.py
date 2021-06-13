d411b86d525ea907b37ce6d0f12c96e7fc80cf7a
green1052 <green1052@protonmail.ch>
2021-06-13 15:59:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,19 @@ from win32_setctime import setctime
 
 def change_file_time(file):
     setctime(file, 0)
-    modTime = mktime(datetime(1970, 1, 1, 9).timetuple())
+    modTime = mktime(datetime(2000, 1, 1, 9).timetuple())
     os.utime(file, (modTime, modTime))
 
 
 def file_write(content):
-    file = open(f"output.txt", "a", encoding="utf-8")
-    file.write(f"{content.replace('﻿', '', len(content))}\n")
-    file.close()
+    with open("output.txt", "a", encoding="utf-8") as file:
+        file.write(f"{content}\n")
+        change_file_time("output.txt")
 
 
 for i in natsorted(os.listdir(os.getcwd())):
     if not i.endswith(".txt"):
         continue
 
-    file = open(i, encoding="utf-8")
-    file_write(file.read())
-    file.close()
-
-change_file_time("output.txt")
+    with open(i, encoding="utf-8") as file:
+        file_write(file.read())
